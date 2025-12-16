27ccae32b78dcf87db7f07571e01535b19dca138
Ke Han <kehan5800@gmail.com>
2025-12-16 15:36:43 -0500

    update script

---
+++ artifact/state-exploration/download_trace.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+CASS_FOLDER_URL="https://drive.google.com/drive/folders/1EElA0esFCRnDRzVsncDE9J1AhYG8UqCo"
+CASS_DIR="hdfs"
+
+HBASE_FOLDER_URL="https://drive.google.com/drive/folders/1EElA0esFCRnDRzVsncDE9J1AhYG8UqCo"
+HBASE_DIR="hdfs"
+
+HDFS_FOLDER_URL="https://drive.google.com/drive/folders/1EElA0esFCRnDRzVsncDE9J1AhYG8UqCo"
+HDFS_DIR="hdfs"
+
+if ! command -v gdown &> /dev/null; then
+  echo "[INFO] gdown not found, installing via python3..."
+  pip3 install --user gdown
+fi
+
+mkdir -p ${CASS_DIR}
+gdown --folder ${CASS_FOLDER_URL} -O ${CASS_DIR}
+
+mkdir -p ${HBASE_DIR}
+gdown --folder ${HBASE_FOLDER_URL} -O ${HBASE_DIR}
+
+mkdir -p ${HDFS_DIR}
+gdown --folder ${HDFS_FOLDER_URL} -O ${HDFS_DIR}
+
+echo "[DONE] Traces downloaded:"
+
